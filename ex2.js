0c413c660c5960910cf7b8d16f7d67572b8ca192
Ana <ana.c.luiz@aluno.senai.br>
2025-05-30 14:38:17 -0300

    first commit

---
+++ ex2.js
@@ -0,0 +1,12 @@
+function ehPar(numero) {
+    if (numero % 2 == 0) {
+        return true
+} else {
+    return false
+}
+}
+
+console.log(ehPar(4))
+console.log(ehPar(17))
+console.log(ehPar(0))
+console.log(ehPar(-8))
